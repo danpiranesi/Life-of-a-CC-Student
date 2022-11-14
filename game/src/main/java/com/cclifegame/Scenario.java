4c015dfe896155cdd6dd9a6579c825510ddac04c
Silvi <117675334+Chromezi@users.noreply.github.com>
2022-11-13 17:51:29 -0700

    Fix the Scenario.java
    Fixed the fields of the scenario.

--- game/src/main/java/com/cclifegame/Scenario.java
+++ game/src/main/java/com/cclifegame/Scenario.java
@@ -1,30 +1,86 @@
 package com.cclifegame;
 
+/**
+ * This is the public scenario class where all the scenario tiles are built in.
+ */
 public class Scenario {
     private String title;
     private int category;
     private String description;
-    private String[] choices;
+    private String choice1;
+    private String choice2;
+    private String result1;
+    private String result2;
     private int[] Lchoice;
     private int[] Rchoice;
 
-    public Scenario(String title, int category, String description, String[] choices, int[] lchoice, int[] rchoice) {
+    /**
+     * @param title The name of this scenario. This will link the scenario to a picture.
+     * @param category The category of this scenario. See MainGame method getCategory for details.
+     * @param description A text description of the scenario (bodytext).
+     * @param choice1 Left choice text.
+     * @param choice2 Right choice text.
+     * @param result1 Left choice result text.
+     * @param result2 Right choice result text.
+     * @param lchoice Left choice's modification on the player stats.
+     * @param rchoice Right choice's modification on the player stats.
+     */
+    public Scenario(String title, int category, String description,
+                    String choice1, String choice2,
+                    String result1, String result2,
+                    int[] lchoice, int[] rchoice) {
+
         this.title = title;
         this.category = category;
         this.description = description;
-        this.choices = choices;
-        Lchoice = lchoice;
-        Rchoice = rchoice;
+        this.choice1 = choice1;
+        this.choice2 = choice2;
+        this.result1 = result1;
+        this.result2 = result2;
+        this.Lchoice = lchoice;
+        this.Rchoice = rchoice;
     }
 
     /**
      * A function for printing the scenarios.
      */
-    public void printScene() {
+    public void printScene(){
         System.out.println(this.getDescription());
-        System.out.println(this.getChoices()[1] + " || " + this.getChoices()[2]);
+        System.out.println("Choice 1: "+this.getChoice1()+" || Choice 2: "+this.getChoice2());
     }
 
+    /**
+     * A function to print the choice descriptions based on player's previous choice.
+     * Used in the MainGame.
+     * @param i the choice (1 or 2).
+     */
+    public void printChoiceDesc(int i){
+        if (i == 1){
+            System.out.println(this.getChoice1());
+        } else if (i == 2) {
+            System.out.println(this.getChoice2());
+        } else {
+            System.out.println("Invalid choice!");
+        }
+    }
+
+    /**
+     * A function to print the choice concequences based on player's previous choice.
+     * Used in the MainGame.
+     * @param i the choice (1 or 2).
+     */
+    public void printConsequence(int i){
+        if (i == 1){
+            System.out.println(this.getResult1());
+        } else if (i == 2) {
+            System.out.println(this.getResult2());
+        } else {
+            System.out.println("Invalid choice!");
+        }
+    }
+
+
+    // Getters and Setters
     public String getTitle() {
         return title;
     }
@@ -45,31 +101,27 @@ public class Scenario {
         return description;
     }
 
-    public void setDescription(String description) {
-        this.description = description;
+    public String getChoice1() {
+        return choice1;
     }
 
-    public String[] getChoices() {
-        return choices;
+    public String getChoice2() {
+        return choice2;
     }
 
-    public void setChoices(String[] choices) {
-        this.choices = choices;
+    public String getResult1() {
+        return result1;
+    }
+
+    public String getResult2() {
+        return result2;
     }
 
     public int[] getLchoice() {
         return Lchoice;
     }
 
-    public void setLchoice(int[] lchoice) {
-        Lchoice = lchoice;
-    }
-
     public int[] getRchoice() {
         return Rchoice;
-    }
-
-    public void setRchoice(int[] rchoice) {
-        Rchoice = rchoice;
     }
 }
